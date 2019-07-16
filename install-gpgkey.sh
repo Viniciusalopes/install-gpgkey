be40fdf4b5e41a1f3b2ed4d5e18ca20f4142f5fa
viniciusalopes <vinicius@viniciusalopes.com.br>
2019-07-16 17:14:25 -0300

    Facilitando a vida...

--- install-gpgkey.sh
+++ install-gpgkey.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Template orgulhosamente criado por (Shell-Base)
-#-----------HEADER-----------------------------------------------------------------|
+# Template orgulhosamente criado por (Shell-Base) tks SlackJeff
+#-----------HEADER-------------------------------------------------------------|
 #AUTOR
-#  Vovolinux <suporte@viniciusalopes.com.br>
+#  Vovolinux <suporte@vovolinux.com.br>
 #
 ##DATA-DE-CRIAÇÃO
 #  16/07/2019 ás 14:19 
@@ -11,7 +11,7 @@
 #  install-gpgkey
 #
 #PEQUENA-DESCRIÇÃO
-#  Script para automatizar a instalação de chave gpg privada em novos computadores
+#  Script para automatizar a instalação de chave gpg privada.
 #
 #LICENÇA
 #  MIT
@@ -21,25 +21,133 @@
 #
 #CHANGELOG
 #
-#----------------------------------------------------------------------------------|
+#------------------------------------------------------------------------------|
 
+# Interrompe a execução em caso de qualquer erro
+set -e
 
-#--------VÁRIAVEIS--------->
-# Aqui vai todas as 
-# váriaveis do programa.
-#-------------------------->
+#------- VÁRIAVEIS -------->
+
+################################################################################
+#   >>> CUIDADO!!! O SCRIPT PODE APAGAR O CONTEUDO DO DIRETÓRIO dir_gpg <<<    #
+# Valor padrão:                                                                #
+# dir_gpg='/root/.gnupg'                                                       #
+dir_gpg='/root/.gnupg'                                                         #
+#   >>> CUIDADO!!! O SCRIPT PODE APAGAR O CONTEUDO DO DIRETÓRIO dir_gpg <<<    #
+################################################################################
+
+### LEU AQUI EM CIMA??? ^^
+
+#------- MENSAGENS -------->
+
+msg_dir_existe='O diretório já existe!'
+msg_arquivo_invalido='[ ERRO ] Arquivo inválido!'
+msg_ate_breve='OK.\nAté breve!'
+
+#----- FIM-MENSAGENS ------>
+
+#----- FIM-VARIAVEIS ------>
 
 
 #--------FUNÇÕES----------->
-# Funções vão aqui!
-#-------------------------->
+ajuda(){
+cat << EOF
+ 
+ INSTRUÇÕES:
+    
+    install-gpgkey <local-da-chave>/<arquivo.key>
+        -> Importa uma chave privada gpg.
 
-#------TESTES-------------->
-# Testes iniciais do
-# seu programa vão
-# neste bloco.
-#-------------------------->
+    install-gpgkey -h 
+    install-gpgkey --help
+        -> Ver esta ajuda.
+  
+EOF
 
+}
+
+# Checagem inicial e obtenção do caminho do arquivo com a chave
+check()
+{
+    # Sem root não vai, amiguinho.
+    [[ "$UID" -ne '0' ]] && { echo "Execute como root."; exit 1 ;}
+
+    # Verificando se o local da chave foi passado
+    if [[ ! -e "$chave" ]]; then
+        echo -e "${msg_arquivo_invalido} (${chave})"
+        ajuda
+        exit 1 
+    fi
+
+    # Verificando se o diretório existe
+    if [[ -d "${dir_gpg}" ]]; then 
+        #dir_gpg já existe
+        echo -e "${msg_dir_existe}"
+        read -ep "Deseja REALMENTE excluir TODOS os arquivos em '${dir_gpg}' ? [s/N ]: " -n 1
+
+        case "$REPLY" in
+             s|S) certeza ;;
+            *) sair  ;;
+        esac
+    fi
+}
+
+certeza()
+{
+    read -ep "Você tem CERTEZA ? [s/N ]: " -n 1
+
+    case "$REPLY" in
+        s|S) absoluta ;;
+        *) sair  ;;
+    esac
+}
+
+absoluta()
+{
+    read -ep "Você tem certeza ABSOLUTA ? [s/N ]: " -n 1
+
+    case "$REPLY" in
+        s|S) instalar ;;
+        *) sair  ;;
+    esac
+
+}
+
+instalar()
+{
+    echo -e 'Então tá. E lá vamos nós...'
+    rm -rfv $dir_gpg/*
+    if ! [[ -d "${dir_gpg}" ]]; then 
+        mkdir -v /root/.gnupg      # cria o dir_gpg
+    fi
+    
+   # Instala
+    echo 'Criando o arquivo '
+    echo 'pinentry-mode loopback' >> ${dir_gpg}/gpg.conf
+    gpg --import $chave
+    echo -e 'Importação concluída!'
+    sair
+}
+
+sair()
+{
+    echo -e "${msg_ate_breve}"
+    exit 0
+}
+#-------------------------->
 
 # Programa começa aqui :)
 
+case $1 in
+    -h|--help) 
+        ajuda 
+        exit 0
+    ;;
+    
+    *)
+        chave="${1}"
+        check
+        inicius
+    ;;
+esac
+exit 0
