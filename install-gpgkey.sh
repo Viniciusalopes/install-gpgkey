c7f7de2b17a7a896f0075bb3ab26bf71a211f108
viniciusalopes <vinicius@viniciusalopes.com.br>
2019-07-16 14:31:02 -0300

    Lets's go!

---
+++ install-gpgkey.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Template orgulhosamente criado por (Shell-Base)
+#-----------HEADER-----------------------------------------------------------------|
+#AUTOR
+#  Vovolinux <suporte@viniciusalopes.com.br>
+#
+##DATA-DE-CRIAÇÃO
+#  16/07/2019 ás 14:19 
+#
+#PROGRAMA
+#  install-gpgkey
+#
+#PEQUENA-DESCRIÇÃO
+#  Script para automatizar a instalação de chave gpg privada em novos computadores
+#
+#LICENÇA
+#  MIT
+#
+#HOMEPAGE
+#  https://github.com/Viniciusalopes/install-gpgkey 
+#
+#CHANGELOG
+#
+#----------------------------------------------------------------------------------|
+
+
+#--------VÁRIAVEIS--------->
+# Aqui vai todas as 
+# váriaveis do programa.
+#-------------------------->
+
+
+#--------FUNÇÕES----------->
+# Funções vão aqui!
+#-------------------------->
+
+#------TESTES-------------->
+# Testes iniciais do
+# seu programa vão
+# neste bloco.
+#-------------------------->
+
+
+# Programa começa aqui :)
+
